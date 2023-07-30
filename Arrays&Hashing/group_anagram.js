321826ce8abf47c4902bdfd1cff1f2048c9a7c55
HaoyL666 <liu_haoyu@berkeley.edu>
2023-07-29 20:29:36 -0700

    finish problem 4

--- Arrays&Hashing/group_anagram.js
+++ Arrays&Hashing/group_anagram.js
@@ -4,7 +4,7 @@
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
- 
+
 
 // Example 1:
 
@@ -18,3 +18,35 @@
 
 // Input: strs = ["a"]
 // Output: [["a"]]
+
+
+/**
+ * Sort - HeapSort Space O(1) | QuickSort Space O(log(K))
+ * Hash Map - Adjacency List
+ * Time O(N * (K * log(K))) | Space O(N * K)
+ * https://leetcode.com/problems/group-anagrams/
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = (strs, map = new Map()) => {
+    if (!strs.length) return [];
+
+    groupWords(strs, map);    /* Time O(N * (K * log(K)) | Space O(N * K) */
+
+    return [...map.values()];/* Time O(N)               | Space O(N * K) */
+};
+
+const groupWords = (strs, map) => {
+    for (const original of strs) {/* Time O(N) */
+        const sorted = reorder(original);/* Time O(K * log(K)) | Space O(K) */
+        const values = map.get(sorted) || [];
+
+        values.push(original);           /*                    | Space O(N) */
+        map.set(sorted, values);         /*                    | Space O(N * K) */
+    }
+}
+
+const reorder = (str) => str
+    .split('')                         /* Time O(K)          | Space O(K) */
+    .sort((a, b) => a.localeCompare(b))/* Time O(K * log(K)) | Space O(1 || log(K)) */
+    .join('');                         /* Time O(K)          | Space O(K) */
