0c53eeeba49daf51b0499a72412e1e5eddcad9f4
Haoyu <liu_haoyu@berkeley.edu>
2023-08-02 23:02:13 -0700

    finish pronlem 12

---
+++ Sliding Windows/longest_substring_without_repeating_characters.js
@@ -0,0 +1,43 @@
+/* 3. Longest Substring Without Repeating Characters */
+
+// Given a string s, find the length of the longest 
+// substring without repeating characters.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * Time O(N) | Space O(N)
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLongestSubstring = function (s) {
+    const set = new Set();
+    let l = 0;
+    let max = 0;
+
+    for (let r = 0; r < s.length; r++) {
+        while (set.has(s[r])) {
+            set.delete(s[l]);
+            l++;
+        }
+        set.add(s[r]);
+        max = Math.max(max, set.size);
+    }
+    return max;
+};
