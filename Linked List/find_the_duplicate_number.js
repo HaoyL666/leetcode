7e29095d64024daab0aa564a340c56114934924a
Haoyu <liu_haoyu@berkeley.edu>
2023-08-18 17:00:53 -0700

    finish approach 1 for find duplicate

--- Linked List/find_the_duplicate_number.js
+++ Linked List/find_the_duplicate_number.js
@@ -1,9 +1,7 @@
 /* 287. Find the Duplicate Number */
 
 // Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-
 // There is only one repeated number in nums, return this repeated number.
-
 // You must solve the problem without modifying the array nums and uses only constant extra space.
 
 
@@ -15,4 +13,20 @@
 // Input: nums = [3,1,3,4,2]
 // Output: 3
 
+
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ * Time O(N * log(N)) | Space O(1)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+    nums.sort((a, b) => a - b);/* Time O(N * log(N)) | HeapSort Space O(1) | QuickSort Space O(log(N)) */
+
+    for (let i = 1; i < nums.length; i++) {/* Time O(N) */
+        if (nums[i - 1] === nums[i]) return nums[i];
+    }
+
+    return -1;
+}
 
