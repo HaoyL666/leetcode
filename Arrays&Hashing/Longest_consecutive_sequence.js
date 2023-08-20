ba4926607378d793f0bf8da777c4da6764bb8226
Haoyu <liu_haoyu@berkeley.edu>
2023-08-20 12:48:53 -0700

    add O(N) time conplexity

--- Arrays&Hashing/Longest_consecutive_sequence.js
+++ Arrays&Hashing/Longest_consecutive_sequence.js
@@ -47,3 +47,33 @@ const search = (nums) => {
 
     return Math.max(maxScore, score);
 }
+
+/**
+ * Hash Set - Intelligent Sequence
+ * Greedy - Max Score
+ * Time O (N) | Space O(N)
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = (nums, maxScore = 0) => {
+    const numSet = new Set(nums);         /* Time O(N) | Space O(N) */
+
+    for (const num of [...numSet]) {    /* Time O(N) */
+        const prevNum = num - 1;
+
+        if (numSet.has(prevNum)) continue;/* Time O(N) */
+
+        let [currNum, score] = [num, 1];
+
+        const isStreak = () => numSet.has(currNum + 1)
+        while (isStreak()) {              /* Time O(N) */
+            currNum++;
+            score++;
+        }
+
+        maxScore = Math.max(maxScore, score);
+    }
+
+    return maxScore;
+}
