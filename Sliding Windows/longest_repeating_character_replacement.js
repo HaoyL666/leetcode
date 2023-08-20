c2acf7a849386e13f3bb49b37b2b85b0d9b41785
Haoyu <liu_haoyu@berkeley.edu>
2023-08-20 15:22:20 -0700

    add longest repeating strings

--- Sliding Windows/longest_repeating_character_replacement.js
+++ Sliding Windows/longest_repeating_character_replacement.js
@@ -3,7 +3,7 @@
 // You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
 // Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
- 
+
 
 // Example 1:
 // Input: s = "ABAB", k = 2
@@ -16,3 +16,31 @@
 // Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 // The substring "BBBB" has the longest repeating letters, which is 4.
 // There may exists other ways to achive this answer too.
+
+/**
+ * https://leetcode.com/problems/longest-repeating-character-replacement/
+ * Time O(((N + 26) * N) * (M - N)) | Space O(1)
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {
+    let result = 0;
+    let count = new Map();
+    let l = 0;
+
+    // use two pointers: left and right to represent a window
+    for (let r = 0; r < s.length; r++) {
+        let len = r - l + 1
+        count.set(s[r], 1 + (count.get(s[r]) || 0))
+
+        if ((len - Math.max(...count.values())) > k) {
+            count.set(s[l], count.get(s[l]) - 1)
+            l++;
+        }
+        len = r - l + 1;
+        result = Math.max(result, len)
+    }
+
+    return result;
+};
