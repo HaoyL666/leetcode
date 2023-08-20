f97ee3b3f6ae200ab7593c72029be12f941e8f9b
Haoyu <liu_haoyu@berkeley.edu>
2023-08-20 12:46:37 -0700

    go back to array, start longest consecutive

---
+++ Arrays&Hashing/Longest_consecutive_sequence.js
@@ -0,0 +1,49 @@
+/* 128. Longest Consecutive Sequence */
+
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+
+
+
+// Example 1:
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+// Example 2:
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
+
+
+/**
+ * Sort - HeapSort Space O(1) | QuickSort Space O(log(K))
+ * Greedy - Max Score
+ * Time O (N * log(N)) | Space O(1)
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = (nums) => {
+    if (!nums.length) return 0;
+
+    nums.sort((a, b) => a - b);/* Time O(N * log(N)) | Space O(1 || log(N)) */
+
+    return search(nums);       /* Time O(N) */
+}
+
+const search = (nums) => {
+    let [maxScore, score] = [1, 1];
+
+    for (let i = 1; i < nums.length; i++) {/* Time O(N) */
+        const isPrevDuplicate = nums[i - 1] === nums[i]
+        if (isPrevDuplicate) continue
+
+        const isStreak = nums[i] === ((nums[i - 1]) + 1)
+        if (isStreak) { score++; continue; }
+
+        maxScore = Math.max(maxScore, score);
+        score = 1;
+    }
+
+    return Math.max(maxScore, score);
+}
