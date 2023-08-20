1a6784f3f223dc71cac5d80165cfc53c161d7ce8
Haoyu <liu_haoyu@berkeley.edu>
2023-08-20 13:37:35 -0700

    revise two pointers

--- Two Pointers/valid_palindrome.js
+++ Two Pointers/valid_palindrome.js
@@ -37,3 +37,36 @@ var isPalindrome = function (s) {
     }
     return isPalindrome(string.slice(1, -1));
 };
+
+/**
+ * 2 Pointer | Midde Convergence
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/valid-palindrome/
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    if (s.length <= 1) return true;
+
+    let [left, right] = [0, s.length - 1];
+    let leftChar, rightChar;
+    while (left < right) {
+        leftChar = s[left];
+        rightChar = s[right];
+
+        // skip char if non-alphanumeric
+        if (!/[a-zA-Z0-9]/.test(leftChar)) {
+            left++;
+        } else if (!/[a-zA-Z0-9]/.test(rightChar)) {
+            right--;
+        } else {
+            // compare letters
+            if (leftChar.toLowerCase() != rightChar.toLowerCase()) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+    }
+    return true;
+};
