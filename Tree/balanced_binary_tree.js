9849305631998c2204629ab0ef27f2a564d9487e
Haoyu <liu_haoyu@berkeley.edu>
2023-09-05 23:53:14 -0700

    finish Balanced Binary Tree

--- Tree/balanced_binary_tree.js
+++ Tree/balanced_binary_tree.js
@@ -5,3 +5,21 @@
 
 // A height-balanced binary tree is a binary tree in which the depth of the two subtrees 
 // of every node never differs by more than one.
+
+const superbalanced = tree => {
+    if (!tree) return true;
+
+    return superbalanced(tree.left) && superbalanced(tree.right) && Math.abs(bstHeight(tree.left) - bstHeight(tree.right)) <= 1;
+
+};
+
+const bstHeight = tree => {
+    if (!tree) return -1;
+
+    const leftHeight = bstHeight(tree.left);
+    const rightHeight = bstHeight(tree.right);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+
+
+};
