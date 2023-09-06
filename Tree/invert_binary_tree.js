642745cac522ffdff14697c5035d3d82cd2e2004
Haoyu <liu_haoyu@berkeley.edu>
2023-09-05 23:39:08 -0700

    finish Invert Binary Tree

--- Tree/invert_binary_tree.js
+++ Tree/invert_binary_tree.js
@@ -1,3 +1,25 @@
 /* 226. Invert Binary Tree */
 
 // Given the root of a binary tree, invert the tree, and return its root.
+
+/**
+ * https://leetcode.com/problems/invert-binary-tree/
+ * TIme O(N) | Space O(N)
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = (root) => {
+    if (root === null) return root;
+
+    return dfs(root);
+}
+
+const dfs = (root) => {
+    const left = invertTree(root.left);
+    const right = invertTree(root.right);
+
+    root.left = right;
+    root.right = left;
+
+    return root;
+}
