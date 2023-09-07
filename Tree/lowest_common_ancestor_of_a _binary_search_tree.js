1dcd338974dbf7cbac69f27fa8a70c299eda4085
Haoyu <liu_haoyu@berkeley.edu>
2023-09-06 21:53:58 -0700

    revise

--- Tree/lowest_common_ancestor_of_a _binary_search_tree.js
+++ Tree/lowest_common_ancestor_of_a _binary_search_tree.js
@@ -24,11 +24,11 @@ Output: 2
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-    const isGreater = (p.val < root.val) && (q.val < root.val);
-    if (isGreater) return lowestCommonAncestor(root.left, p, q);
 
-    const isLess = (root.val < p.val) && (root.val < q.val);
-    if (isLess) return lowestCommonAncestor(root.right, p, q);
+    if ((p.val < root.val) && (q.val < root.val)) return lowestCommonAncestor(root.left, p, q);
 
+    if ((root.val < p.val) && (root.val < q.val)) return lowestCommonAncestor(root.right, p, q);
+
+    // if p.val === root.value || q.val === root.value || (p.val < root.val && root.val < q.val) || (q.val < root.val && root.val < p.val)
     return root;
 };
