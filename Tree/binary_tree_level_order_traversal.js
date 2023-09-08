6fc05003c23efc8e1cbd89b75142cd964baa4216
Haoyu <liu_haoyu@berkeley.edu>
2023-09-07 18:16:20 -0700

    finish levelOrder method 2

--- Tree/binary_tree_level_order_traversal.js
+++ Tree/binary_tree_level_order_traversal.js
@@ -48,3 +48,31 @@ var levelOrder = function (root) {
 
     return result;
 };
+
+
+/**
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * Time O(N) | Space O(H)
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var levelOrder = function (root, level = 0, levels = []) {
+    // base case
+    if (root === null) return levels;
+
+    // if the current level is not in the levels array, add it
+    const isLastNode = level === levels.length;
+    if (isLastNode) levels.push([]);
+
+    // push the current node's value to the current level
+    levels[level].push(root.val);
+
+    return dfs(root, level, levels); // Time O(N) | Space O(H)
+}
+
+const dfs = (root, level, levels) => {
+    if (root.left) levelOrder(root.left, (level + 1), levels);
+    if (root.right) levelOrder(root.right, (level + 1), levels);
+
+    return levels;
+}
