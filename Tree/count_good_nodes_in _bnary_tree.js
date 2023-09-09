12947f1a409075ee809adebadb27c9b17c4d1906
Haoyu <liu_haoyu@berkeley.edu>
2023-09-09 02:13:42 -0700

    finish count nodes

--- Tree/count_good_nodes_in _bnary_tree.js
+++ Tree/count_good_nodes_in _bnary_tree.js
@@ -15,13 +15,14 @@
  * @return {number}
  */
 var goodNodes = function (root, max = -Infinity, total = [0]) {
+    // the reason we use an array for total is because we want to pass the same array to all recursive calls
     count(root, max, total);
 
     return total[0]
 };
 
 const count = (root, max, total) => {
-    if (isBaseCase) return 0;
+    if (root === null) return 0;
 
     return dfs(root, max, total);
 }
@@ -30,6 +31,7 @@ const dfs = (root, max, total) => {
     const isGood = max <= root.val
     if (isGood) total[0]++;
 
+    // max is the max value of the path from the root to the current node
     max = Math.max(max, root.val);
 
     count(root.left, max, total);
