ae7d6618dca4d4d7d4b7909e18f773315e69e2f0
Haoyu <liu_haoyu@berkeley.edu>
2023-09-09 03:00:21 -0700

    add solution1

--- Tree/validate_binary_search_tree.js
+++ Tree/validate_binary_search_tree.js
@@ -5,7 +5,31 @@
 
 // A valid BST is defined as follows:
 
-// The left subtree of a node contains only nodes with keys less than the node's key.
-// The right subtree of a node contains only nodes with keys greater than the node's key.
-// Both the left and right subtrees must also be binary search trees.
+// 1.The left subtree of a node contains only nodes with keys less than the node's key.
+// 2.The right subtree of a node contains only nodes with keys greater than the node's key.
+// 3.Both the left and right subtrees must also be binary search trees.
 
+
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ * Time O(N) | Space O(H)
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root, min = -Infinity, max = Infinity) {
+    if (root === null) return true;
+
+    // check condition 1 and 2
+    const isInvalid = (root.val <= min) || (max <= root.val);
+    if (isInvalid) return false;
+
+    return dfs(root, min, max);
+};
+
+const dfs = (root, min, max) => {
+    const left = isValidBST(root.left, min, root.val);
+    const right = isValidBST(root.right, root.val, max);
+
+    // check condition 3
+    return left && right;
+}
