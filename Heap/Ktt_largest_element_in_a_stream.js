c77256b51e70913450debaec74d13aade4e418bb
Haoyu <liu_haoyu@berkeley.edu>
2023-09-17 01:37:02 -0700

    add heapclass

--- Heap/Ktt_largest_element_in_a_stream.js
+++ Heap/Ktt_largest_element_in_a_stream.js
@@ -4,3 +4,49 @@
 
 // KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
 // int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+/** 
+ * https://leetcode.com/problems/kth-largest-element-in-a-stream/
+ * Time O(N * (K * log(K))) | Space O(K)
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     * @constructor
+     */
+    constructor(k, nums) {
+        this.k = k
+        this.minHeap = new MinPriorityQueue();
+
+        nums.forEach((num) => this.add(num))
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val, { minHeap } = this) {
+        const isUnderCapacity = minHeap.size() < this.k;
+        if (isUnderCapacity) {
+            minHeap.enqueue(val);
+
+            return this.top();
+        }
+
+        const isLarger = this.top() < val;
+        if (isLarger) {
+            minHeap.dequeue()
+            minHeap.enqueue(val);
+        }
+
+        return this.top();
+    }
+
+    top({ minHeap } = this) {
+        return minHeap.front()?.element || 0
+    }
+}
