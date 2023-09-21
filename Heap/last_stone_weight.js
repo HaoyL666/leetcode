e2a94406e0dbcf178190bc8d89c2357bc173599e
Haoyu <liu_haoyu@berkeley.edu>
2023-09-20 23:43:18 -0700

    finish stone

--- Heap/last_stone_weight.js
+++ Heap/last_stone_weight.js
@@ -26,3 +26,38 @@ Explanation:
 // Example 2:
 Input: stones = [1]
 Output: 1
+
+
+/**
+ * https://leetcode.com/problems/last-stone-weight/
+ * Time O(N * log(N)) | Space O(N)
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+    const maxHeap = getMaxHeap(stones)
+
+    shrink(maxHeap)
+
+    return !maxHeap.isEmpty()
+        ? maxHeap.front().element
+        : 0
+};
+
+const getMaxHeap = (stones, maxHeap = new MaxPriorityQueue()) => {
+    for (const stone of stones) {
+        maxHeap.enqueue(stone)
+    }
+
+    return maxHeap
+}
+
+const shrink = (maxHeap) => {
+    while (1 < maxHeap.size()) {
+        const [x, y] = [maxHeap.dequeue().element, maxHeap.dequeue().element]
+        const difference = x - y;
+
+        const isPositive = 0 < difference
+        if (isPositive) maxHeap.enqueue(difference);
+    }
+}
