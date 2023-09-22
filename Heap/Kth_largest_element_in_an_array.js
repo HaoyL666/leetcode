8581be595d08b1bb4407de00504a1faf444ae6be
Haoyu <liu_haoyu@berkeley.edu>
2023-09-22 00:18:19 -0700

    finish using heap

--- Heap/Kth_largest_element_in_an_array.js
+++ Heap/Kth_largest_element_in_an_array.js
@@ -31,3 +31,24 @@ var findKthLargest = function (nums, k) {
         .slice(k - 1)
         .shift()
 };
+
+// use max heap
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ * Time O(N * log(K)) | Space O(K)
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+    const minHeap = new MinPriorityQueue()
+
+    for (const num of nums) {
+        minHeap.enqueue(num);
+
+        const isAtCapacity = k < minHeap.size();
+        if (isAtCapacity) minHeap.dequeue();
+    }
+
+    return minHeap.front().element
+};
